cb91534aa02a683c565698cc3ca8d543e7667038
samluethy <sluethy@instapathbio.com>
2021-05-28 12:57:10 -0400

    steps per axis

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -806,7 +806,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 500 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 5039.370079, 5039.370079, 12598.4252, 100}
 
 /**
  * Default Max Feed Rate (mm/s)
