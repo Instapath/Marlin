f5fe13c008e8dc606e21bb18966abbfa4f91314d
Tom Szymczyk <tszymczyk@instapathbio.com>
2021-06-08 15:41:45 -0500

    added cal values for servocity 3x-disk joystick

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -3764,9 +3764,9 @@
   //#define INVERT_JOY_Z  // Enable if Z direction is reversed
 
   // Use M119 with JOYSTICK_DEBUG to find reasonable values after connecting:
-  #define JOY_X_LIMITS { 5600, 8190-100, 8190+100, 10800 } // min, deadzone start, deadzone end, max
-  #define JOY_Y_LIMITS { 5600, 8250-100, 8250+100, 11000 }
-  #define JOY_Z_LIMITS { 4800, 8080-100, 8080+100, 11550 }
+  #define JOY_X_LIMITS { 5, 8300-100, 8300+100, 16367 } // min, deadzone start, deadzone end, max
+  #define JOY_Y_LIMITS { 5, 9300-100, 9300+100, 16367 }
+  #define JOY_Z_LIMITS { 5, 8100-100, 8100+100, 16367 }
   #define JOYSTICK_DEBUG
 #endif
 
