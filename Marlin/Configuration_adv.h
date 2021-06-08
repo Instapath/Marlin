0af48e8025a606245829ec8ee3e343c9d3b33c96
Tom Szymczyk <tszymczyk@instapathbio.com>
2021-06-08 14:33:43 -0500

    joystick configs

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -3752,12 +3752,12 @@
 /**
  * Analog Joystick(s)
  */
-//#define JOYSTICK
+#define JOYSTICK
 #if ENABLED(JOYSTICK)
-  #define JOY_X_PIN    5  // RAMPS: Suggested pin A5  on AUX2
-  #define JOY_Y_PIN   10  // RAMPS: Suggested pin A10 on AUX2
-  #define JOY_Z_PIN   12  // RAMPS: Suggested pin A12 on AUX2
-  #define JOY_EN_PIN  44  // RAMPS: Suggested pin D44 on AUX2
+  #define JOY_X_PIN    PA3
+  #define JOY_Y_PIN   PA2
+  #define JOY_Z_PIN   PA1
+  #define JOY_EN_PIN  PC14
 
   //#define INVERT_JOY_X  // Enable if X direction is reversed
   //#define INVERT_JOY_Y  // Enable if Y direction is reversed
@@ -3767,7 +3767,7 @@
   #define JOY_X_LIMITS { 5600, 8190-100, 8190+100, 10800 } // min, deadzone start, deadzone end, max
   #define JOY_Y_LIMITS { 5600, 8250-100, 8250+100, 11000 }
   #define JOY_Z_LIMITS { 4800, 8080-100, 8080+100, 11550 }
-  //#define JOYSTICK_DEBUG
+  #define JOYSTICK_DEBUG
 #endif
 
 /**
